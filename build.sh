a7d573a8ea5d760aa70cdb72c3c44ecdb26d70a7
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-13 20:17:10 +0200

    add docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env
 
-docker build -t data2knowledge/wtl-api-node:0.5.3 .
+docker build -t data2knowledge/wtl-api-node:0.5.5 .
 docker rm -f wtl-api-node
 docker run -dt --name wtl-api-node data2knowledge/wtl-api-node:0.5.3
